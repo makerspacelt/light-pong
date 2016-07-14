73cdc40ce24c4668e19eee80f0febda9dfe90590
dariuskt <dariuskt@users.noreply.github.com>
2016-07-14 23:22:49 +0300

    adding openwrt setup functions

--- beacon/openwrt.sh
+++ beacon/openwrt.sh
@@ -3,6 +3,7 @@
 
 configure_wifi() {
 	echo -e '\n### Configure wifi =========================================\n' \
+	&& uci set 'network.lan.ipaddr=192.168.4.1' \
 	&& uci set 'wireless.radio0.disabled=0' \
 	&& uci set 'wireless.radio0.channel=8' \
 	&& uci set 'wireless.@wifi-iface[0]=wifi-iface' \
@@ -26,7 +27,24 @@ configure_ip() {
 	ifconfig br-lan:0 192.168.4.2 up
 }
 
+configure_startup()
+{
+	local f=/etc/init.d/light-pong
+	echo '#!/bin/sh /etc/rc.common' > $f
+	echo 'START=99' >>$f
+	echo 'boot() { start; }' >>$f
+	echo 'start() { /root/openwrt.sh; }' >>$f
+	chmod a+x $f /root/openwrt.sh
+	$f enable
+	sync
+}
+
 start_broadcaster() {
 	ncat --broker -knl 2048
 }
 
+install_dependencies
+configure_wifi
+configure_ip
+configure_startup
+start_broadcaster
